5b051f50f3b3c50fc88d685bc3225ab00fb610b4
Janen Khan <janenfkhan@gmail.com>
2020-07-28 15:11:26 +0000

    google api try

--- app.py
+++ app.py
@@ -8,26 +8,46 @@ from flask_pymongo import PyMongo
 from flask import redirect
 import os
 from dotenv import load_dotenv
+from flask import url_for, session
+from authlib.integrations.flask_client import OAuth
+from datetime import timedelta
+from auth_decorator import login_required
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-
 # first let's load environment variables in .env
 load_dotenv()
-
-
-#then store environment variables with new names
+# then store environment variables with new names 
 MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
-
+# URI of database
 # name of database
 app.config['MONGO_DBNAME'] = 'studentplanner'
 # URI of database
 app.config['MONGO_URI'] = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@cluster0.gwiez.mongodb.net/studentplanner?retryWrites=true&w=majority'
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
+
 mongo = PyMongo(app)
+
 # -- Routes section --
 # INDEX
 @app.route('/')
@@ -56,7 +76,6 @@ def new_event():
     if request.method == "GET":
         return render_template('new_events.html')
     else:
-
         event_name = request.form['event_name']
         date = request.form['date']
         description = request.form['description']
@@ -65,7 +84,39 @@ def new_event():
         events = mongo.db.schedule
         #Add to the database
         events.insert({'event': event_name, "date": date, "description": description, 'types': types})
-        
         collection = mongo.db.schedule
         events = collection.find({})
     return render_template('show_events.html', events = events, time=datetime.now())
+
+@app.route('/')
+@login_required
+def hello_world():
+    email = dict(session)['profile']['email']
+    return f'Hello, you are logged in as {email}!'
+
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
