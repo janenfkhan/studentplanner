27d82018a122a406ef207e9f34d1f4c605bb1560
Mahel Napo <napomahel@gmail.com>
2020-07-29 15:38:20 +0000

    idk anymore

--- quickstart.py
+++ quickstart.py
@@ -11,9 +11,9 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+    # """Shows basic usage of the Google Calendar API.
+    # Prints the start and name of the next 10 events on the user's calendar.
+    # """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
